07b8dee1e7fb435526e3a81fad19fb155b0f11c7
ABDESSALAM GHER <106890970+DDOSooS@users.noreply.github.com>
2024-12-16 17:43:20 +0000

    ex04 cpp Modul01 is being Done

--- cpp__01/ex04/main.cpp
+++ cpp__01/ex04/main.cpp
@@ -2,27 +2,113 @@
 #include <fstream>
 #include <string>
 
-using namespace std;
-
-int main(int ac, char **av)
+int ft_strlen(const char *str)
 {
-    if (ac != 4 || av[1][0] == '\0' || av[2][0] == '\0' || av[3][0] == '\0')
-    {
-        cout << "try Again with this Format: <fileName> <s1> <s2>\n";
-        return (1);
-    }
-    fstream new_file; 
-    new_file.open(av[1],ios::out);  
-    if(!new_file) 
-    {
-        cout<<"File creation failed\n";
-    }
-    else
-    {
-        cout<<"New file created\n";
-        new_file<<"Learning File handling\n";
-        new_file.close(); 
-    }   
-    return (0);
+    int counter;
+
+    counter = -1;
+    while (str[++counter]);
+    return (counter);
 }
 
+bool isFileEmpty(std::fstream &file)
+{
+    return file.peek() == std::fstream::traits_type::eof();
+}
+
+void replaceOccurrence(char *fileName, std::string *lines, const char *s1, const char *s2, int fileCount)
+{
+    std::ofstream file(fileName, std::ios::out);
+
+    if (!file.is_open())
+    {
+        std::cout << "Couldn't open the file\n";
+        return;
+    }
+    for(int i = 0; i < fileCount; i++)
+    {   
+        std::string currentLine = lines[i];
+        size_t oldIndex = 0;
+        size_t res = currentLine.find(s1);
+        while (res != std::string::npos)
+        {
+            file << currentLine.substr(oldIndex, res - oldIndex);
+            file << s2;
+            oldIndex = res + ft_strlen(s1);
+            res = currentLine.find(s1, oldIndex);
+        }
+        if (oldIndex < currentLine.length())
+            file << currentLine.substr(oldIndex);
+        file << std::endl;
+    }
+    file.close();
+}
+
+void getLines(const char *fileName, std::string *lines)
+{
+    std::ifstream file(fileName);
+    if (file.is_open())
+    {
+        std::string line;
+        int i = 0;
+        while (std::getline(file, line))
+        {
+            lines[i++] = line;
+        }
+        file.close();
+    }
+    else
+        std::cerr << "Error opening file: " << fileName << "\n";
+}
+
+int countLines(const char *fileName)
+{
+    std::ifstream file(fileName);
+    if (!file.is_open())
+    {
+        std::cerr << "Error opening file: " << fileName << "\n";
+        return 0;
+    }
+    int counter = 0;
+    std::string line;
+    while (std::getline(file, line))
+        counter++;
+    file.close();
+    return counter;
+}
+
+int checkFile(const char *fileName)
+{
+    std::fstream new_file(fileName, std::ios::in);
+    if (!new_file)
+    {
+        std::cerr << "File creation failed\n";
+        return 0;
+    }
+    if (isFileEmpty(new_file))
+    {
+        std::cout << "File is empty\n";
+        new_file.close();
+        return 0;
+    }
+    new_file.close();
+    return (1);
+}
+
+int main(int argc, char **argv)
+{
+    if (argc != 4 || argv[1][0] == '\0' || argv[2][0] == '\0' || argv[3][0] == '\0')
+    {
+        std::cout << "Try again with this format: <program> <fileName> <S1> <S2>\n";
+        return 1;
+    }
+    const char *fileName = argv[1];
+    if (! checkFile(fileName))
+        return (0);
+    int lineCount = countLines(fileName);
+    std::string *lines = new std::string[lineCount];
+    getLines(fileName, lines);
+    replaceOccurrence(argv[1],lines, argv[2], argv[3], lineCount);
+    delete[] lines;
+    return 0;
+}
