0d4638d4ac4cb0a27772fad8df62b5ca824f6578
ABDESSALAM GHER <106890970+DDOSooS@users.noreply.github.com>
2024-12-27 16:09:33 +0000

    -another Way of solvinf Diamond Prob but with Creating two Object

--- cpp__03/ex03/DiamondTrap.cpp
+++ cpp__03/ex03/DiamondTrap.cpp
@@ -1,25 +1,25 @@
 #include "./DiamondTrap.hpp"
 
-DiamondTrap::DiamondTrap(std::string &name):ClapTrap(name) , FragTrap(name), ScavTrap(name)
+DiamondTrap::DiamondTrap(std::string &name):FragTrap(name), ScavTrap(name)
 {
     std::cout << "DiamondTap Object is being Created\n";
     std::string clap_name = name + "_clap_name";
-    ClapTrap::setName(clap_name);
+    FragTrap::setName(clap_name);
     this->_name = name;
-    this->setAttackDamage(FragTrap::getAttackDamage());
-    this->setEnergyPoints(ScavTrap::getEnergyPoints());
-    this->setHitPoints(FragTrap::getHitPoints());
+    this->FragTrap::setAttackDamage(FragTrap::getAttackDamage());
+    this->FragTrap::setEnergyPoints(ScavTrap::getEnergyPoints());
+    this->FragTrap::setHitPoints(FragTrap::getHitPoints());
 }
 
 void DiamondTrap::attack(const std::string& target)
 {
-    ScavTrap::attack(target);
+    this->ScavTrap::attack(target);
 }
 
 
 void DiamondTrap::whoAmI()
 {
-    std::cout   << "ClapTrap object's name of the Diamond Trap object is << " << ClapTrap::getName() << " - Diamod Trap's Name :"
+    std::cout   << "ClapTrap object's name of the Diamond Trap object is << " << FragTrap::getName() << " - Diamod Trap's Name :"
                 << this->_name << std::endl;
 }
 
--- cpp__03/ex03/main.cpp
+++ cpp__03/ex03/main.cpp
@@ -5,9 +5,9 @@ int main() {
     std::string name2 = "IMAD";
     DiamondTrap p(name);
 
-    p.attack(name2);
-    p.takeDamage(5);
-    p.beRepaired(15);
+    // p.000::attack(name2);
+    p.ScavTrap::takeDamage(5);
+    p.FragTrap::beRepaired(15);
     p.whoAmI();    
 
     return 0;
