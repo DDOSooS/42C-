b548c07b06246e4a535d09cfa27572c6ccea2ebc
ABDESSALAM GHER <106890970+DDOSooS@users.noreply.github.com>
2024-12-27 16:11:50 +0000

    CPP MODULE 03

--- cpp__03/ex03/main.cpp
+++ cpp__03/ex03/main.cpp
@@ -5,9 +5,9 @@ int main() {
     std::string name2 = "IMAD";
     DiamondTrap p(name);
 
-    // p.000::attack(name2);
-    p.ScavTrap::takeDamage(5);
-    p.FragTrap::beRepaired(15);
+    p.takeDamage(5);
+    p.beRepaired(15);
+    p.attack(name2);
     p.whoAmI();    
 
     return 0;
